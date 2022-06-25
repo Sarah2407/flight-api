93c29a5cb67d83ed84ea4813e468a093e7b0d071
Sarah Mensah <mansahmensah18@gmail.com>
2022-06-25 17:42:43 +0000

    added example model

--- models/Flight.js
+++ models/Flight.js
@@ -1,4 +1,10 @@
 const allFlights = [
+    {
+        title: "Flight to Togo",
+        time: "9pm",
+        price: "10.00",
+        date: "20th May"
+    }
 ];
 
 module.exports = allFlights;
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.flights);
+router.get('', controller.flights);
 router.post('/create-flight', controller.newFlight);
 router.patch('/:id', controller.updateFlight);
 router.delete('/:id', controller.deleteFlight);
